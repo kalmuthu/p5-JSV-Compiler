18b54b1c261531770efc803ecf28d01fc851f2d9
Yoshitaro Makise <yoshizow@turtlewalk.org>
2015-11-08 01:49:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+JSV::Compiler - It's new $module
+
+# SYNOPSIS
+
+    use JSV::Compiler;
+
+# DESCRIPTION
+
+JSV::Compiler is ...
+
+# LICENSE
+
+Copyright (C) Yoshitaro Makise.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Yoshitaro Makise <yoshizow@turtlewalk.org>
---
+++ lib/JSV/Compiler.pm
@@ -0,0 +1,39 @@
+package JSV::Compiler;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSV::Compiler - It's new $module
+
+=head1 SYNOPSIS
+
+    use JSV::Compiler;
+
+=head1 DESCRIPTION
+
+JSV::Compiler is ...
+
+=head1 LICENSE
+
+Copyright (C) Yoshitaro Makise.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yoshitaro Makise E<lt>yoshizow@turtlewalk.orgE<gt>
+
+=cut
+
